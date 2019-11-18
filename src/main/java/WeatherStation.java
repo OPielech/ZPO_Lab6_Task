f34e6afe4606be9e1aa212d1799a6150893380e4
OPielech <56829225+OPielech@users.noreply.github.com>
2019-11-18 14:05:31 +0100

    new version

--- src/main/java/WeatherStation.java
+++ src/main/java/WeatherStation.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 
 public class WeatherStation {
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String city;
         String nameOfCity;
         Double temperature;
@@ -22,46 +22,46 @@ public class WeatherStation {
 
 
         System.out.println("Please enter the name of city");
-        city=scanner.nextLine();
+        city = scanner.nextLine();
 
-        Gson gson=new Gson();
-        StringBuffer response=new StringBuffer();
-        String url="http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=7c738d8fcb3d09197280291bccdd812d";
+        Gson gson = new Gson();
+        StringBuffer response = new StringBuffer();
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=7c738d8fcb3d09197280291bccdd812d";
 
         try {
             URL obj = new URL(url);
-            HttpURLConnection connection=(HttpURLConnection) obj.openConnection();
+            HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
             connection.setRequestMethod("GET");
-            int responseCode=connection.getResponseCode();
+            int responseCode = connection.getResponseCode();
 
             String inputLine;
-            BufferedReader in=new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            while ((inputLine=in.readLine())!=null){
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
 
-        }catch (MalformedURLException e){
+        } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        Map map=gson.fromJson(String.valueOf(response),Map.class);
-        Map map1=gson.fromJson(String.valueOf(map.get("main")),Map.class);
+        Map map = gson.fromJson(String.valueOf(response), Map.class);
+        Map map1 = gson.fromJson(String.valueOf(map.get("main")), Map.class);
 
-        nameOfCity=map.get("name").toString();
-        temperature=Double.valueOf(map1.get("temp").toString());
-        pressure=Double.valueOf(map1.get("pressure").toString());
-        humidity=Double.valueOf(map1.get("humidity").toString());
-        minTemperature=Double.valueOf(map1.get("temp_min").toString());
-        maxTemperature=Double.valueOf(map1.get("temp_max").toString());
+        nameOfCity = map.get("name").toString();
+        temperature = Double.valueOf(map1.get("temp").toString());
+        pressure = Double.valueOf(map1.get("pressure").toString());
+        humidity = Double.valueOf(map1.get("humidity").toString());
+        minTemperature = Double.valueOf(map1.get("temp_min").toString());
+        maxTemperature = Double.valueOf(map1.get("temp_max").toString());
 
-        System.out.println("Name of city: "+nameOfCity);
-        System.out.printf("Temperature: %.1f\n",(temperature-273));
-        System.out.println("Pressure: "+pressure);
-        System.out.println("Humidity: "+humidity);
-        System.out.printf("Minimum temperature: %.1f\n",(minTemperature-273));
-        System.out.printf("Maximum temperature: %.1f\n",(maxTemperature-273));
+        System.out.println("Name of city: " + nameOfCity);
+        System.out.printf("Temperature: %.1f\n", (temperature - 273));
+        System.out.println("Pressure: " + pressure);
+        System.out.println("Humidity: " + humidity);
+        System.out.printf("Minimum temperature: %.1f\n", (minTemperature - 273));
+        System.out.printf("Maximum temperature: %.1f\n", (maxTemperature - 273));
 
     }//end of main
 }//end of class
